91ff3209d5edbd8eb3f02e289a88db2782977203
Alex Wang <selltrends@gmail.com>
2016-07-13 09:12:58 +0800

    add registration.php

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2016 atopt. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'atopt_ru_ru',
+    __DIR__
+);
